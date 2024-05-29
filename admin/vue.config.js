c3d3efe08708542d2e1f997f4cbc5d9196621c4b
mysteryjun <834269929@qq.com>
2024-05-29 17:04:49 +0800

    commit

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,86 +1,87 @@
-const path = require('path')
+const path = require('path');
 
-function resolve (dir) {
-  return path.join(__dirname, dir)
+function resolve(dir) {
+    return path.join(__dirname, dir);
 }
 
 module.exports = {
-  publicPath: './',
-  lintOnSave: true,
-  // 打包时不生成.map文件
-  productionSourceMap: false,
-  chainWebpack: (config) => {
-    // 配置静态资源图片
-    config.module.rules.delete('images')
-    // 清除svg默认的处理方式
-    config.module.rules.delete('svg')
-    config.plugin('html').tap(args => {
-      args[0].title = '后台管理系统'
-      return args
-    })
-  },
-  configureWebpack: {
-    devtool: 'source-map',
-    resolve: {
-      extensions: ['.js', '.vue', '.json'], // 当引入文件时默认先找.js后缀的文件，没找到再从左往右继续
-      alias: {
-        vue$: 'vue/dist/vue.esm.js',
-        '@': resolve('src')
-      }
+    publicPath: './',
+    lintOnSave: true,
+    // 打包时不生成.map文件
+    productionSourceMap: false,
+    chainWebpack: config => {
+        console.log(config);
+        // 配置静态资源图片
+        config.module.rules.delete('images');
+        // 清除svg默认的处理方式
+        config.module.rules.delete('svg');
+        config.plugin('html').tap(args => {
+            args[0].title = '后台管理系统1';
+            return args;
+        });
     },
-    performance: {
-      hints: false
-    },
-    module: {
-      rules: [
-        {
-          test: /\.svg$/,
-          loader: 'svg-sprite-loader',
-          include: [resolve('src/static/icons')],
-          options: {
-            symbolId: 'icon-[name]'
-          }
+    configureWebpack: {
+        devtool: 'source-map',
+        resolve: {
+            extensions: ['.js', '.vue', '.json'], // 当引入文件时默认先找.js后缀的文件，没找到再从左往右继续
+            alias: {
+                vue$: 'vue/dist/vue.esm.js',
+                '@': resolve('src'),
+            },
         },
-        {
-          test: /\.(png|jpe?g|gif|webp|svg)(\?.*)?$/,
-          exclude: [resolve('src/static/icons')],
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 4096,
-                fallback: {
-                  loader: 'file-loader',
-                  options: {
-                    name: 'img/[name].[hash:8].[ext]'
-                  }
-                }
-              }
-            }
-          ]
-        }
-      ]
-    }
-  },
-  devServer: {
-    // https: true,
-    host: '0.0.0.0',
-    port: 8888,
-    proxy: {
-      [process.env.VUE_APP_BASE_API]: {
-        target: 'http://127.0.0.1:3000',
-        changeOrigin: true,
-        pathRewrite: {
-          ['^' + process.env.VUE_APP_BASE_API]: '/api/v1'
-        }
-      },
-      '/img_url': {
-        target: 'http://127.0.0.1:3000',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/img_url': ''
-        }
-      }
-    }
-  }
-}
+        performance: {
+            hints: false,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.svg$/,
+                    loader: 'svg-sprite-loader',
+                    include: [resolve('src/static/icons')],
+                    options: {
+                        symbolId: 'icon-[name]',
+                    },
+                },
+                {
+                    test: /\.(png|jpe?g|gif|webp|svg)(\?.*)?$/,
+                    exclude: [resolve('src/static/icons')],
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 4096,
+                                fallback: {
+                                    loader: 'file-loader',
+                                    options: {
+                                        name: 'img/[name].[hash:8].[ext]',
+                                    },
+                                },
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    devServer: {
+        // https: true,
+        host: '0.0.0.0',
+        port: 8888,
+        proxy: {
+            [process.env.VUE_APP_BASE_API]: {
+                target: 'http://127.0.0.1:3000',
+                changeOrigin: true,
+                pathRewrite: {
+                    ['^' + process.env.VUE_APP_BASE_API]: '/api/v1',
+                },
+            },
+            '/img_url': {
+                target: 'http://127.0.0.1:3000',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/img_url': '',
+                },
+            },
+        },
+    },
+};
--- front-api/app/config/config.js
+++ front-api/app/config/config.js
@@ -1,6 +1,6 @@
 // 配置文件
 module.exports = {
-  environment: 'dev', // prod 为生产环境 dev 为开发环境
-  secret: 'cai', // jwt密钥
-  version: 'v1' // api版本
-}
+    environment: 'dev', // prod 为生产环境 dev 为开发环境
+    secret: 'cai', // jwt密钥
+    version: 'v1', // api版本
+};
